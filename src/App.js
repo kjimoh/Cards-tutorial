c0f40dab0e17dd479abb83a356fe797dd9316b7f
Kamaldeen Jimoh <jimohkamal446@gmail.com>
2023-03-29 13:04:25 +0100

    deleted imports

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./Home";
-import CardDetails from "./CardDetails";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/:cardId">
-          <CardDetails />
-        </Route>
-      </Switch>
-    </Router>
-  );
+ return <div></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,24 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-import cardReducer from "./cardSlice";
-
-export const store = configureStore({
-  reducer: {
-    cards: cardReducer,
-  },
-});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+ <React.StrictMode>
+  <App />
+ </React.StrictMode>,
+ document.getElementById("root")
 );
-
-reportWebVitals();
